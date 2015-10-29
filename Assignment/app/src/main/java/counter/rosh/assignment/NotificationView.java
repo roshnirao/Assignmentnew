44c51767e18e03a3d772c06b64aaaf47c729df50
roshnirao <roshnirao28@gmail.com>
2015-10-29 23:14:34 +0530

    An android app

---
+++ Assignment/app/src/main/java/counter/rosh/assignment/NotificationView.java
@@ -0,0 +1,15 @@
+package counter.rosh.assignment;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hj on 10/29/2015.
+ */
+public class NotificationView extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
